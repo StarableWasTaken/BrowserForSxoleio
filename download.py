4f866dcebfb4f8426521c62bae50d56151c06178
whackkquackk <badshark945@gmail.com>
2026-05-08 00:50:50 +0300

    Add files via upload

---
+++ download.py
@@ -0,0 +1,17 @@
+import subprocess
+import sys
+
+packages = ["PyQt5", "PyQtWebEngine"]
+
+for package in packages:
+    print(f"Installing {package}...")
+    
+    subprocess.check_call([
+        sys.executable,
+        "-m",
+        "pip",
+        "install",
+        package
+    ])
+
+print("\nEverything installed successfully.")
